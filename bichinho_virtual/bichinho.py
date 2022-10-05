adc41751825316ed4e7989e0337a7ebe1d2e01d6
DavidSilveira80 <davidpscontato@gmail.com>
2022-10-05 11:49:43 -0300

    Implementada classe Tamagushi e primeiros atributos e métodos.

--- bichinho_virtual/bichinho.py
+++ bichinho_virtual/bichinho.py
@@ -9,3 +9,34 @@ o Humor do nosso tamagushi, este humor é uma combinação entre os atributos Fo
 ou seja, um campo calculado, então não devemos criar um atributo para armazenar esta informação
 por que ela pode ser calculada a qualquer momento.
 """
+
+class Tamagushi:
+    def __init__(self, nome: str, fome: int, saude: int, idade: int) -> None:
+        self.nome = nome
+        self.fome = fome
+        self. saude = saude
+        self.idade = idade
+
+    def alterar_nome(self, nome):
+        self.nome = nome
+
+    def alterar_fome(self, fome):
+        self.fome = fome
+
+    def alterar_saude(self, saude):
+        self.saude = saude
+
+    def alterar_idade(self, idade):
+        self.idade = idade
+
+    def retornar_nome(self):
+        return self.nome
+
+    def retornar_fome(self):
+        self.fome
+
+    def retornar_saude(self):
+        return self.saude
+
+    def retornar_idade(self):
+        return self.idade
